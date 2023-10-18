21edd5e3b625fcc5ea78793ce0be05c9cf23229d
ThanadolU <dolthanadolu@gmail.com>
2023-10-18 22:25:39 +0700

    change type of password-input tag to password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
             <label className="label-name">Referee ID</label>
             <input className="textbox id-input" type="text" placeholder="Type your ID" />
             <label className="label-name">Password</label>
-            <input className="textbox password-input" type="text" placeholder="Type your password" />
+            <input className="textbox password-input" type="password" placeholder="Type your password" />
             <a className="forgot-password" href="">Forgot password</a>
             <button>Sign In</button>
         </div>
