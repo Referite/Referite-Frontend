04e936a846b8a9ebaae6f4f662465e735852c4f6
ThanadolU <dolthanadolu@gmail.com>
2023-10-19 21:49:59 +0700

    🚧 handling event from id-input and password-input

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,37 @@
-import '../styles/Login.css'
+import { useState } from 'react';
 import referiteLogo from '../assets/images/referite_logo.png'
+import '../styles/Login.css'
 
 function Login() {
+  const [refereeID, setRefereeID] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    // console.log(refereeID, password)
+  }
+
   return (
     <div className="login">
-        <img src={referiteLogo} alt="referite logo" />
-        <div className="box">
-            <label className="label-name">Referee ID</label>
-            <input className="textbox id-input" type="text" placeholder="Type your ID" />
-            <label className="label-name">Password</label>
-            <input className="textbox password-input" type="password" placeholder="Type your password" />
-            <a className="forgot-password" href="">Forgot password</a>
-            <button>Sign In</button>
-        </div>
+      <img src={referiteLogo} alt="referite logo" />
+      <div className="box">
+        <label className="label-name">Referee ID</label>
+        <input className="textbox id-input"
+          type="text"
+          placeholder="Type your ID"
+          value={refereeID}
+          onChange={(e) => setRefereeID(e.target.value)}
+        />
+        <label className="label-name">Password</label>
+        <input className="textbox password-input"
+          type="password" 
+          placeholder="Type your password"
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <a className="forgot-password" href="">Forgot password</a>
+        <button className='sign-in' onClick={handleSubmit}>Sign In</button>
+      </div>
     </div>
   )
 }
