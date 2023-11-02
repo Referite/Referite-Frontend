3ba06d941d7a8fe058921913e87011bba27a7fed
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2023-11-02 18:28:59 +0700

    :art: Removed some unused code

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,8 @@
 import '../styles/SideBar.css';
 import sidebar_logo from '../assets/images/sidebar_logo.png';
 import return_button from '../assets/images/return_button.png';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function Sidebar () {
-
-    const navigate = useNavigate();
 
     return (
         <div className="side-bar">
