db1783ce41175b80b725a5e4b39b9f0fd0ac4892
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2023-11-07 00:04:12 +0700

    :sparkles: Added update medal data to IOC function

--- src/assets/services/RecordMedal.ts
+++ src/assets/services/RecordMedal.ts
@@ -8,3 +8,13 @@ export async function getMessage(body: any){
         throw error; // Re-throw the error for the caller to handle
     }
 } 
+
+export async function UpdateToIOC(body: any) {
+    try {
+        const msg = await axios.post(`http://127.0.0.1:8000/api/record/medal/update`, body);
+        return msg.status;
+
+    } catch (error) {
+        throw error;
+    }
+}
