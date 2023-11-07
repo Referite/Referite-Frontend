94330bd56aa84779ff91db0153e17bdada4ef91c
Isaraa <isaraa.p@ku.th>
2023-11-07 23:24:29 +0700

    Adjust medal status function to get symbol based on status

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,24 @@
 import '../styles/SideBar.css';
 import sidebar_logo from '../assets/images/sidebar_logo.png';
-import return_button from '../assets/images/return_button.png';
+import information_logo from '../assets/images/information_icon.png'
+import logout_logo from '../assets/images/logout_logo.png'
 import { Link } from 'react-router-dom'
+
 export default function Sidebar () {
 
     return (
         <div className="side-bar">
             <div className="logo">
                 <Link to="/">
-                    <img src={sidebar_logo} className="logo-pic"/>
+                    <img src={sidebar_logo} className="sidebar-logo-pic"/>
                 </Link>
+            </div>
+            <div className='information'>
+                <img src={information_logo} className="information-logo-pic" />
             </div>
             <div className="return-button">
                 <Link to="/login">
-                    <img src={return_button} className='return-pic'/>
+                    <img src={logout_logo} className="logout-logo-pic" />
                 </Link>
             </div>
         </div>
---
+++ src/routes/SportSchedule.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { SportScheduleObject } from "../interfaces/SportSchedule";
+
+
+const sportNameURL = 'http://127.0.0.1:8000/api/schedule/sport';
+const sportScheduleURL = 'http://127.0.0.1:8000/api/schedule/all'; // sport schedule API endpoint URL
+
+
+export const getSportName = (setSportNameList: React.Dispatch<React.SetStateAction<Array<string>>>, 
+    setSportNameListStatus: React.Dispatch<React.SetStateAction<boolean>>) => {
+    axios.get(sportNameURL)
+    .then((response) => {
+        console.log(response.data.sport_list);
+        setSportNameListStatus(true);
+        setSportNameList(response.data.sport_list);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+export const getSportSchedule = async (setSportScheduleList: React.Dispatch<React.SetStateAction<SportScheduleObject[]>>, 
+    setSportScheduleListStatus: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setSportScheduleListStatus(false);
+    await axios.get(sportScheduleURL)
+    .then((response) => {
+        console.log(response.data.schedule_list);
+        setSportScheduleListStatus(true);
+        setSportScheduleList(response.data.schedule_list);
+
+        // const testArray: Array<string> = [];
+        // for (let i = 0; i < response.data.schedule_list.length; i++) {
+        //     if (!testArray.includes(response.data.schedule_list[i].datetime)) {
+        //         testArray.push(response.data.schedule_list[i].datetime)
+        //     }
+        // }
+        // console.log(testArray)
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+};
