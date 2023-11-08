65e098b6f7355f3302988367cf4265b0a6b22f9e
ThanadolU <dolthanadolu@gmail.com>
2023-11-08 15:48:09 +0700

    removed comment code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -111,13 +111,6 @@ function Home() {
                   return (
                     <tr key={rec.sport_id}>
                         <td style={{display: 'flex', justifyItems: 'center', alignItems:'center'}}>
-                          {/* <div style={{marginTop: '5px', width: '4vw'}}>
-                            {rec.sport_icon && rec.sport_icon[0] && (
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 504 504" xmlSpace="preserve">
-                                <path fill="#242752" d={rec.sport_icon[0]}></path>
-                              </svg>
-                            )}
-                          </div> */}
                           <SportIcon rec={rec} viewBox={"0 0 504 504"} />
                           <p style={{marginLeft: '6px'}}>{rec.sport_name}</p>
                         </td>
@@ -130,53 +123,6 @@ function Home() {
                     return (
                       <tr key={rec.sport_id}>
                         <td style={{display: 'flex', justifyItems: 'center', alignItems:'center'}}>
-                          {/* <div style={{marginTop: '5px', width: '4vw'}}>
-                            <circle cx="137.56" cy="261.55" r="2.5"></circle>
-                            <circle cx="137.56" cy="228.61" r="2.5"></circle>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 504 504" xmlSpace="preserve">
-                              <circle cx="137.56" cy="261.55" r="2.5"></circle>
-                              <circle cx="137.56" cy="228.61" r="2.5"></circle>
-                              <circle cx="139.88" cy="279.62" r="2.5"></circle>
-                              <circle cx="155.21" cy="259.29" r="2.5"></circle>
-                              <circle cx="152.76" cy="228.61" r="2.5"></circle>
-                              <circle cx="178.68" cy="228.61" r="2.5"></circle>
-                              <circle cx="166.68" cy="228.61" r="2.5"></circle>
-                              <circle cx="153.7" cy="228.61" r="2.5"></circle>
-                              <circle cx="145.16" cy="228.61" r="2.5"></circle>
-                              <circle cx="159.66" cy="228.61" r="2.5"></circle>
-                              <circle cx="161.3" cy="228.61" r="2.5"></circle>
-                              <circle cx="129.96" cy="228.61" r="2.5"></circle>
-                              <circle cx="173.24" cy="228.61" r="2.5"></circle>
-                              <path fill="#242752" d={rec.sport_icon[0]}></path>
-                              <circle cx="192.52" cy="226.06" r="2.5"></circle>
-                              <circle cx="188.32" cy="241.36" r="2.5"></circle>
-                              <circle cx="177.89" cy="259.44" r="2.5"></circle>
-                              <circle cx="374.04" cy="262.23" r="2.5"></circle>
-                              <circle cx="315.68" cy="262.64" r="2.5"></circle>
-                              <circle cx="124.68" cy="268.98" r="2.5"></circle>
-                              <circle cx="311.48" cy="277.94" r="2.5"></circle>
-                              <circle cx="337.32" cy="258.86" r="2.5"></circle>
-                              <circle cx="325.31" cy="284.73" r="2.5"></circle>
-                              <circle cx="326.1" cy="244.56" r="2.5"></circle>
-                              <path fill="#242752" d={rec.sport_icon[1]}></path>
-                              <circle cx="393.89" cy="252.38" r="2.5"></circle>
-                              <circle cx="389.24" cy="267.79" r="2.5"></circle>
-                              <circle cx="379.32" cy="235.02" r="2.5"></circle>
-                              <path fill="#242752" d={rec.sport_icon[2]}></path>
-                              <circle cx="330.76" cy="306.34" r="2.5"></circle>
-                              <circle cx="351.24" cy="260.19" r="2.5"></circle>
-                              <circle cx="366.44" cy="242.45" r="2.5"></circle>
-                              <circle cx="364.11" cy="224.38" r="2.5"></circle>
-                              <circle cx="110.1" cy="251.62" r="2.5"></circle>
-                              <circle cx="114.75" cy="236.21" r="2.5"></circle>
-                              <circle cx="366.44" cy="275.39" r="2.5"></circle>
-                              <circle cx="348.78" cy="244.71" r="2.5"></circle>
-                              <circle cx="358.84" cy="298.2" r="2.5"></circle>
-                              <circle cx="350.3" cy="273.15" r="2.5"></circle>
-                              <circle cx="342.7" cy="293.5" r="2.5"></circle>
-                              <circle cx="344.34" cy="226.32" r="2.5"></circle>
-                            </svg>
-                          </div> */}
                           <BeachVolleyballIcon rec={rec} viewBox={"0 0 504 504"} />
                           <p style={{marginLeft: '6px'}}>{rec.sport_name}</p>
                         </td>
@@ -188,20 +134,6 @@ function Home() {
                     return (
                       <tr>
                         <td style={{display: 'flex', justifyItems: 'center', alignItems:'center'}}>
-                          {/* <div style={{marginTop: '5px', marginBottom: 'auto', width: '4vw'}}>
-                              <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 140 160" xmlSpace="preserve" className="white "> 
-                                <g className="emblem"> 
-                                {rec.sport_icon && rec.sport_icon[0] && (
-                                  <path d={rec.sport_icon[0]} fill="#242752">
-                                  </path>
-                                )}
-                                {rec.sport_icon && rec.sport_icon[1] && (
-                                  <path d={rec.sport_icon[1]} fill="#242752">
-                                  </path>
-                                )}
-                                </g>
-                              </svg>
-                          </div> */}
                           <CeremoniesIcon rec={rec} viewBox={"0 0 140 160"} />
                           <p style={{marginLeft: '6px'}}>Ceremonies</p>
                         </td>
@@ -213,16 +145,6 @@ function Home() {
                     return (
                       <tr key={rec.sport_id}>
                         <td style={{display: 'flex', justifyItems: 'center', alignItems:'center'}}>
-                          {/* <div style={{marginTop: '5px', width: '4vw'}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 504 504" xmlSpace="preserve">
-                            {rec.sport_icon && rec.sport_icon[0] && (
-                              <path fill="#242752" d={rec.sport_icon[0]}></path>
-                            )}
-                            {rec.sport_icon && rec.sport_icon[1] && (
-                              <path fill="#242752" d={rec.sport_icon[1]}></path>
-                            )}
-                            </svg>
-                          </div> */}
                           <SportIcon2 rec={rec} viewBox={"0 0 504 504"} />
                           <p style={{marginLeft: '6px'}}>{rec.sport_name}</p>
                         </td>
