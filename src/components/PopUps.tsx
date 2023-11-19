215d5cdd92960d467594f26f75fd075824b6401d
Tanabodee-Y <tanabodee.y@ku.th>
2023-11-20 01:43:18 +0700

    Fixed popups responsive, etc.

--- src/components/PopUps.tsx
+++ src/components/PopUps.tsx
@@ -3,6 +3,7 @@ import incorrect_icon from '../assets/images/referite_icon/incorrect_icon.png';
 import warning_icon from '../assets/images/referite_icon/warning_icon.png'
 import complete_icon from '../assets/images/referite_icon/complete_icon.png'
 import { UpdateToIOC } from "../assets/services/RecordMedal";
+import '../styles/PopUps.css';
 
 
 function findTypeIdByTypeName(typeName: string, sportTypes: any) {
@@ -19,13 +20,16 @@ function findTypeIdByTypeName(typeName: string, sportTypes: any) {
 export function ErrorPopup (message: string) {
     Swal.fire({
         title: `<span class="incorrect-title-class" style="color: #b04d4c; font-size: 3vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> Incorrect Condition! </span>`,
-        html: ` <span> ${message} </span>`,
-        iconHtml: `<img src="${incorrect_icon}" alt="Custom Icon"  style="width: 6vw;">`,
+        html: `<span style="font-size: 1.7vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> ${message} </span>`,
+        iconHtml: `<img src="${incorrect_icon}" alt="Custom Icon" style="width: auto; max-width: 100px; height: auto;">`,
         confirmButtonText: 'Try Again',
         confirmButtonColor: '#b04d4c',
         customClass: {
-            icon: 'no-border'
-          }
+            icon: 'no-border',
+            popup: 'popup-icon',
+            confirmButton: 'my-swal-button', 
+            cancelButton: 'my-swal-button'
+        }
       });
 }
 
@@ -33,7 +37,7 @@ export function ErrorPopup (message: string) {
 // confirmation popup
 export function ConfirmationPopup (medalValues: any, sport:any, selectedType: string, sport_id: any) {
     let medalsHtml = medalValues.map((item: any) => `
-            <div>
+            <div style="font-size: 1.7vw; font-family: 'TH SarabunPSK'; font-weight: bold;">
                 <span> ${item.country}: </span>&nbsp;&nbsp;
                 <span>Gold: ${item.medal.gold}</span>&nbsp;
                 <span>Silver: ${item.medal.silver}</span>&nbsp;
@@ -44,17 +48,17 @@ export function ConfirmationPopup (medalValues: any, sport:any, selectedType: st
                 title: `<span style="color: #4d5499; font-size: 3vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> Carefully Check! </span>`,
                 html: `
                     <p> 
-                        <span style="color: #4d5499; font-size: 2vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> 
+                        <span style="color: #4d5499; font-size: 2.5vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> 
                             Sport: 
                         </span> 
-                        ${sport.sport_name} 
+                        <span style="font-size: 1.7vw; font-family: 'TH SarabunPSK';"> ${sport.sport_name} </span> 
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <span style="color: #4d5499; font-size: 2vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> 
+                        <span style="color: #4d5499; font-size: 2.5vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> 
                             Event: 
                         </span> 
-                        ${selectedType}
+                        <span style="font-size: 1.7vw; font-family: 'TH SarabunPSK';"> ${selectedType} </span> 
                     </p>
-                    <div style="overflow-y: auto; max-height: 17vh">
+                    <div style="overflow-y: auto; max-height: 15vh">
                         ${medalsHtml}
                     </div>
                     <br><br>
@@ -67,7 +71,9 @@ export function ConfirmationPopup (medalValues: any, sport:any, selectedType: st
                 confirmButtonText: 'Confirm',
                 confirmButtonColor: '#4d5499',
                 customClass: {
-                    container: 'confirmation-popup-container'
+                    container: 'confirmation-popup-container',
+                    confirmButton: 'my-swal-button', 
+                    cancelButton: 'my-swal-button'
                 }
             }).then(async (result) => {
                 if (result.isConfirmed) {
@@ -94,14 +100,17 @@ export function ConfirmationPopup (medalValues: any, sport:any, selectedType: st
 export function WarningPopup (message: string, medalValues: any, sport:any, selectedType: string, sport_id: any) {
     Swal.fire({ 
         title: `<span style="color: #de9f00; font-size: 3vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> Warning! </span>`,
-        html: ` <span> ${message} </span>`,
-        iconHtml: `<img src="${warning_icon}" alt="Custom Icon"  style="width: 6vw;">`,
+        html: ` <span style="font-size: 1.7vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> ${message} </span>`,
+        iconHtml: `<img src="${warning_icon}" alt="Custom Icon"  style="width: auto; max-width: 100px; height: auto;">`,
         showCancelButton: true,
         cancelButtonText: 'Cancel',
         confirmButtonText: 'Confirm',
         confirmButtonColor: '#4d5499',
         customClass: {
-            icon: 'no-border'
+            icon: 'no-border',
+            popup: 'popup-icon',
+            confirmButton: 'my-swal-button', 
+            cancelButton: 'my-swal-button'
           }
     }).then(async (result) => {
         if (result.isConfirmed) {
@@ -115,10 +124,14 @@ export function WarningPopup (message: string, medalValues: any, sport:any, sele
 export function successPopup (status: number) {
     if (status === 200) {
         Swal.fire({
-            iconHtml: `<img src="${complete_icon}" alt="Custom Icon"  style="width: 6vw;">`,
+            iconHtml: `<img src="${complete_icon}" alt="Custom Icon" style="width: auto; max-width: 100px; height: auto;">`,
             title: `<span style="color: #363b7a; font-size: 3vw; font-family: 'TH SarabunPSK'; font-weight: bold;"> Recorded Successfully! </span>`,
             showConfirmButton: false,
-            timer: 1500
+            width: '35vw',
+            timer: 1500,
+            customClass: {
+                popup: 'popup-icon'
+            }
         });
         // Set a timeout to reload the page after the popup closes
         setTimeout(() => {
